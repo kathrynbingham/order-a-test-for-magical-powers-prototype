80e1242c3afd7e1d91d70ee9deb27e9fc494829a
kathrynbingham <61273641+kathrynbingham@users.noreply.github.com>
2026-04-27 14:03:12 +0000

    routes changes

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,32 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+
+router.post('/symptoms-answer', function (req, res) {
+  const data = req.session.data
+  const Doyouhavesymptoms = data.Doyouhavesymptoms
+
+  if (Doyouhavesymptoms === "Yes") {
+
+    res.redirect('/details')
+
+  } else if (Doyouhavesymptoms === "No") {
+
+    res.redirect('/ineligible')
+
+  } else if (Doyouhavesymptoms === "I have no idea") {
+
+    res.redirect('/ineligible')
+
+  } else {
+
+    // No answer selected, return to question
+    res.redirect('/symptoms')
+
+  }
+})
+
+
+
+
 module.exports = router
