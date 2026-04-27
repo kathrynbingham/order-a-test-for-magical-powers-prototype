7a43c0a2b0727473c2896e00dee8d4e63c901e90
kathrynbingham <61273641+kathrynbingham@users.noreply.github.com>
2026-04-27 09:39:19 +0000

    add service name

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'Service name goes here',
+  serviceName: 'Order a test for magical powers',
 
   // Port to run the prototype on locally
   port: 3000
